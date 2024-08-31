2a794e975d90bd6223f56b7e3ac45d1f4f51744e
zenin5479 <zenin5479@gmail.com>
2024-08-31 15:30:07 +0300

    Update 0.1 Обновлен метод

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,90 @@
       /// </summary>
       private void InitializeComponent()
       {
+         LabelMilliseconds = new System.Windows.Forms.Label();
+         LabelCalls = new System.Windows.Forms.Label();
+         TextBoxCalls = new System.Windows.Forms.TextBox();
+         LabelSpent = new System.Windows.Forms.Label();
+         TextBoxAverage = new System.Windows.Forms.TextBox();
+         LabelAverage = new System.Windows.Forms.Label();
          SuspendLayout();
+         // 
+         // LabelMilliseconds
+         // 
+         LabelMilliseconds.AutoSize = true;
+         LabelMilliseconds.Location = new System.Drawing.Point(118, 36);
+         LabelMilliseconds.Name = "LabelMilliseconds";
+         LabelMilliseconds.Size = new System.Drawing.Size(104, 21);
+         LabelMilliseconds.TabIndex = 56;
+         LabelMilliseconds.Text = "миллисекунд";
+         // 
+         // LabelCalls
+         // 
+         LabelCalls.AutoSize = true;
+         LabelCalls.Location = new System.Drawing.Point(663, 9);
+         LabelCalls.Name = "LabelCalls";
+         LabelCalls.Size = new System.Drawing.Size(72, 21);
+         LabelCalls.TabIndex = 55;
+         LabelCalls.Text = "вызывов";
+         // 
+         // TextBoxCalls
+         // 
+         TextBoxCalls.Location = new System.Drawing.Point(605, 6);
+         TextBoxCalls.Name = "TextBoxCalls";
+         TextBoxCalls.Size = new System.Drawing.Size(52, 29);
+         TextBoxCalls.TabIndex = 54;
+         TextBoxCalls.Text = "10";
+         // 
+         // LabelSpent
+         // 
+         LabelSpent.AutoSize = true;
+         LabelSpent.Location = new System.Drawing.Point(471, 9);
+         LabelSpent.Name = "LabelSpent";
+         LabelSpent.Size = new System.Drawing.Size(128, 21);
+         LabelSpent.TabIndex = 53;
+         LabelSpent.Text = "Затраченное на ";
+         // 
+         // TextBoxAverage
+         // 
+         TextBoxAverage.Location = new System.Drawing.Point(12, 33);
+         TextBoxAverage.Name = "TextBoxAverage";
+         TextBoxAverage.Size = new System.Drawing.Size(100, 29);
+         TextBoxAverage.TabIndex = 52;
+         // 
+         // LabelAverage
+         // 
+         LabelAverage.AutoSize = true;
+         LabelAverage.Location = new System.Drawing.Point(12, 9);
+         LabelAverage.Name = "LabelAverage";
+         LabelAverage.Size = new System.Drawing.Size(336, 21);
+         LabelAverage.TabIndex = 51;
+         LabelAverage.Text = "Cреднее арифметическое время выполнения";
          // 
          // Form1
          // 
-         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(779, 522);
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         ClientSize = new System.Drawing.Size(744, 522);
+         Controls.Add(LabelMilliseconds);
+         Controls.Add(LabelCalls);
+         Controls.Add(TextBoxCalls);
+         Controls.Add(LabelSpent);
+         Controls.Add(TextBoxAverage);
+         Controls.Add(LabelAverage);
+         Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
          Name = "Form1";
          Text = "Парсинг";
          Load += Form1_Load;
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.Label LabelMilliseconds;
+      private System.Windows.Forms.Label LabelCalls;
+      private System.Windows.Forms.TextBox TextBoxCalls;
+      private System.Windows.Forms.Label LabelSpent;
+      private System.Windows.Forms.TextBox TextBoxAverage;
+      private System.Windows.Forms.Label LabelAverage;
    }
 }
