84a792617ac96edb46c6b91244edbad027ad3a0b
zenin5479 <zenin5479@gmail.com>
2024-09-02 19:19:27 +0300

    Update 1.0 Обновлен метод

--- Form1.cs
+++ Form1.cs
@@ -26,8 +26,8 @@ namespace WindowsFormsParse
          string name = a.GetType().AssemblyQualifiedName;
          if (name != null) RichTextBoxOne.AppendText(name);
 
-         //RichTextBoxOne.AppendText( i.GetType().Name + Environment.NewLine);
-         //RichTextBoxOne.AppendText(i.GetType().FullName + Environment.NewLine);
+         RichTextBoxOne.AppendText(i.GetType().Name + Environment.NewLine);
+         RichTextBoxOne.AppendText(i.GetType().FullName + Environment.NewLine);
       }
 
       private void ButtonParseTwo_Click(object sender, EventArgs e)
